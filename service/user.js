18ea5f9227753c2b0db7ea3fd5a330f9d3561f5f
伍鑫 <wuxin>
2020-06-17 16:04:10 +0800

    refactor: user

--- service/user.js
+++ service/user.js
@@ -1,16 +1,22 @@
 const { query } = require("../db")
-
+const getUserByName = async(username) => {
+  const rows = await query('SELECT * FROM user WHERE username = ?', username)
+  if (rows.length === 0) {
+    return Promise.resolve(false)
+  }
+  return Promise.resolve(rows[0])
+}
 const login = async (params = {}) => {
   if(!params.username || !params.password) {
     const result = { code: -1, message: '用户名和密码不能为空' }
     return Promise.resolve(result)
   }
-  const rows = await query('SELECT * FROM user WHERE username = ?', params.username)
-  if(rows.length === 0) {
+  const user = await getUserByName(params.username)
+  if(!user) {
     const result = { code: -1, message: '用户名不存在' }
     return Promise.resolve(result)
   }
-  if(rows[0].password !== params.password) {
+  if(user.password !== params.password) {
     const result = { code: -1, message: '密码错误' }
     return Promise.resolve(result)
   }
@@ -22,8 +28,8 @@ const register = async (params = {}) => {
     const result = { code: -1, message: '用户名和密码不能为空' }
     return Promise.resolve(result)
   }
-  const rows = await query('SELECT * FROM user WHERE username = ?', params.username)
-  if(rows.length !== 0) {
+  const user = await getUserByName()
+  if(user) {
     return Promise.resolve({ code: -1, message: '用户名已存在' })
   }
   const result = await query('INSERT INTO user SET ?', params)
